056d773284dc080537f83170a554cfe2166c6130
Jannis <mail-z@gmx.de>
2016-11-18 13:47:02 +0100

    First commit

---
+++ Name.js
@@ -0,0 +1,17 @@
+
+	var Namen= {
+			"Name": {
+				"Vorname": [
+					"Anton",
+					"Berta",
+					"Annika",
+					"Casper",
+					"Daniel",
+					"Domenik",
+					"Emil",
+					"Friedolin",
+					"Gisela",
+					"Hans"
+				]
+			}
+	}
